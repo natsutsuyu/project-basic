58d803116a64b36035fa986c6153de49bf9c69a3
natsutsuyu <osakinana1994@gmail.com>
2024-01-18 23:26:36 +0400

    creating function-constructor Task with id, description and cost fields. Also added getters for private fields.

---
+++ index.js
@@ -0,0 +1,43 @@
+function Task(id, description, cost) {
+  if (new.target===undefined) {
+    throw new Error('Cannot create an instance of an abstract class.');
+  }
+
+  this._id = "id";
+  if(typeof description==='string'){
+    this._description=description;
+  }
+  else{
+    throw new Error("Incorrect type of description!");
+  }
+  if(cost>=0&&typeof cost==='number'){
+    this._cost=cost;
+  }
+  else{
+    throw new Error("Incorrect type or value of cost!");
+  }
+
+  Object.defineProperty(this, 'id',{
+    get: function(){
+      return id;
+    }
+  })
+
+  Object.defineProperty(this, 'description',{
+    get: function(){
+      return description;
+    }
+  })
+
+  Object.defineProperty(this, cost, {
+    get: function(){
+      return cost;
+    }
+  })
+}
+const task = new Task('1', "Name of task", 50);
+
+console.log(task.id);
+
+
+
